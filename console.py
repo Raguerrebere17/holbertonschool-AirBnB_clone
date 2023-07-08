7c7b66dbe274a39a6da22986ea4f4bfe28c49ddd
Mariana Echeto <echetomariana@gmail.com>
2023-07-08 03:19:51 -0300

    update console

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ A module cmd that contains the entry point of the command interpreter
 """
 import cmd
 import models
+# from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,8 +12,10 @@ class HBNBCommand(cmd.Cmd):
     class initialization
     """
     prompt = "(hbnb) "
+    storage = models.storage
+    storage.reload()
 
-    def arg_checker(self, arg):
+    def arg_checker(self, args):
         if len(args) == 0:
             print('** class name missing **')
             return False
@@ -50,41 +53,37 @@ class HBNBCommand(cmd.Cmd):
             class and saves it to the JSON file
         """
         args = arg.split()
-        # if len(args) == 0:
-        #     print('** class name missing **')
-        # else:
-        #     class_name = args[0]
-        #     if class_name not in models.dict_class:
-        #         print('** class doesn\'t exist **')
-        if arg_checker(args):
-            instance = models.dict_class[class_name]()
+        if self.arg_checker(args):
+            instance = models.dict_class[args[0]]()
             instance.save()
             print(instance.id)
 
     def do_show(self, arg):
         """
-        Prints the string representation of an instance
-
-        Args:
-            arg (str): class & id.
+        Prints the string representation of an instance based on the class name and id.
         """
+        if not arg:
+            print("** class name missing **")
+            return
+
         args = arg.split()
-        # if len(args) == 0:
-        #     print('** class name missing **')
-        # else:
-        #     class_name = args[0]
-        #     if class_name not in models.dict_class:
-        #         print('** class doesn\'t exist **')
-        if arg_checker(args):
-            if len(args) < 2:
-                print('** instance id missing **')
-            else:
-                instance_id = args[1]
-                instance = models.storage.get(class_name, instance_id)
-                if instance is None:
-                    print('** no instance found **')
-                else:
-                    print(instance)
+        class_name = args[0]
+
+        if class_name not in models.dict_class:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = f"{class_name}.{instance_id}"
+
+        if key in models.storage.all():
+            print(models.storage.all()[key])
+        else:
+            print("** no instance found **")
 
     def do_destroy(self, arg):
         """
@@ -94,25 +93,19 @@ class HBNBCommand(cmd.Cmd):
             arg (str): class & id
         """
         args = arg.split()
-        # if len(args) == 0:
-        #     print('** class name missing **')
-        # else:
-        #     class_name = args[0]
-        #     if class_name not in models.dict_class:
-        #         print('** class doesn\'t exist **')
-        if arg_checker(args):
+        if self.arg_checker(args):
             if len(args) < 2:
                 print('** instance id missing **')
             else:
-                instance_id = args[1]
                 # Deletes the instance of the specified class
                 # and id and saves the changes to the JSON file
-                instance = models.storage.get(class_name, instance_id)
-                if instance is None:
-                    print('** no instance found **')
-                else:
-                    instance.delete()
+                key = f"{args[0]}.{args[1]}"
+                temp = models.storage.all()  # temp is self.__object
+                if key in temp:
+                    del (temp[key])
                     models.storage.save()
+                else:
+                    print("** no instance found **")
 
     def do_update(self, arg):
         """
@@ -122,31 +115,23 @@ class HBNBCommand(cmd.Cmd):
             arg (str): class & id
         """
         args = arg.split()
-        # if len(args) == 0:
-        #     print('** class name missing **')
-        # else:
-        #     class_name = args[0]
-        #     if class_name not in models.dict_class:
-        #         print('** class doesn\'t exist **')
-        if arg_checker(args):
+        if self.arg_checker(args):
             if len(args) < 2:
                 print('** instance id missing **')
             else:
                 instance_id = args[1]
-                instance = models.storage.get(class_name, instance_id)
-                if instance is None:
+                key = "{}.{}".format(args[0], args[1])
+                temp = models.storage.all()  # temp is self.__object
+                if key not in temp:
                     print('** no instance found **')
                 elif len(args) < 3:
                     print('** attribute name missing **')
                 elif len(args) < 4:
                     print('** value missing **')
                 else:
-                    attr_name = args[2]
-                    attr_value = args[3]
-                    # Updates the instance attribute with the
-                    # specified value and saves the changes to the JSON file
-                    setattr(instance, attr_name, attr_value)
-                    instance.save()
+                    new_obj = temp.get(key)
+                    setattr(new_obj, args[2], args[3])
+                    models.storage.save()
 
     def do_all(self, arg):
         """
@@ -166,9 +151,9 @@ class HBNBCommand(cmd.Cmd):
                 return
             # Prints the string representation
             # of all instances of the specified class
-            instances = models.storage.all(class_name)
-        for instance in instances.values():
-            print(instance)
+            instances = models.storage.all()
+            for instance in instances.values():
+                print(instance)
 
 
 if __name__ == '__main__':
